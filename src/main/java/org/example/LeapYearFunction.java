63788d4613bd480947cffbcb325c38066e2ea7aa
ttchau134 <ttchau@hiof.no>
2022-10-27 11:35:23 +0200

    Changed function LeapYearFunction. Expected failure

--- src/main/java/org/example/LeapYearFunction.java
+++ src/main/java/org/example/LeapYearFunction.java
@@ -3,7 +3,7 @@ package org.example;
 public class LeapYearFunction {
     public static int checkLeapYear(int year)
     {
-        boolean divides_by_4 = (year%4==0);
+        boolean divides_by_4 = (year%3==0);
         boolean divides_by_400 = (year%400==0);
         boolean divides_by_100 = (year%100==0);
         boolean divides_by_4000 = (year%4000==0);
