d2502e403746ef43ca210251b642be2e3b4e4487
ttchau134 <ttchau@hiof.no>
2022-10-27 10:35:38 +0200

    Testing automated testing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args)
     {
-        int year = 2100;
+        int year = 2180;
 
         if (LeapYearFunction.checkLeapYear(year)==1)
             System.out.printf("Year %d is a leap year\n", year);
