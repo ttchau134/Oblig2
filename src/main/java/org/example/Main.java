fb7cc9bb605a32fb5ee48fd8a4ab19f011505461
ttchau134 <ttchau@hiof.no>
2022-10-27 11:08:34 +0200

    Changed year data in main to test gradlew

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args)
     {
-        int year = 2120;
+        int year = 2126;
 
         if (LeapYearFunction.checkLeapYear(year)==1)
             System.out.printf("Year %d is a leap year\n", year);
